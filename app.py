0c94e17dcb38b244e4ebb958e9b0f0fcffa1e20f
felipenunezb <f.nunezb@gmail.com>
2022-05-20 17:25:41 -0400

    simple plotly

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from collections import defaultdict
 import streamlit as st
 import pandas as pd
 
 from dia.extract import get_all_data
+from dia.analyze import get_single_results
+
+from aux_fx import get_dict_references
 
 st.set_page_config(page_icon="👩‍🏫", page_title="DIA", layout="wide")
 
@@ -20,17 +24,25 @@ def main():
         f = st.sidebar.file_uploader("Seleccione los documentos a subir", type=['xls'], accept_multiple_files=True)
         
         if f:
-            st.write(f)
             
             info_list, results_list = get_all_data(f)
             pruebas = [fl.name[23 + (fl.name[23:-4].find('_') + 1): 23 + (fl.name[23:-4].find('_') + 1) + (fl.name[23 + (fl.name[23:-4].find('_') + 1):-4].find('_'))] for fl in f]
                 
+            ref_dict = get_dict_references(info_list, pruebas)
             
-            st.write(info_list)
-            st.write(pruebas)
-            
-            st.write(results_list[0])
-            
+            view_results(ref_dict, results_list)
         
+        
+def view_results(ref_dict, results_list):
+    
+    curso = st.selectbox(label = 'Curso', options=sorted(list(ref_dict.keys())))
+    prueba = st.selectbox(label = 'Prueba', options=ref_dict[curso].keys())
+    
+    result_ix = ref_dict[curso][prueba]
+    
+    results = get_single_results(df=results_list[result_ix], df_nm=curso)
+    
+    st.plotly_chart(results['kde'])
+    
         
 main()
