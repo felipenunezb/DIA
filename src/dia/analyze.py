7e38a2bb6b6d67be1961224670024fd2a83dac9d
felipenunezb <f.nunezb@gmail.com>
2022-05-21 22:33:09 -0400

    group comparison

--- src/dia/analyze.py
+++ src/dia/analyze.py
@@ -5,16 +5,45 @@ from collections import defaultdict
 import plotly.figure_factory as ff
 
 
-def get_single_results(df: pd.DataFrame, df_nm: str) -> dict:
+def get_single_results(df: pd.DataFrame) -> dict:
     
-    #results = defaultdict(lambda: defaultdict())
-    
-    #ncols = len(df.columns)
-    cols = list(df.columns[1:])
+    #ejes
+    cols = list(df.columns[2:])
 
     fig = ff.create_distplot([df[c] for c in cols], cols, show_hist=False, show_rug=False)
       
     return {'kde': fig}
+
+def get_comparison(df: pd.DataFrame):
+    
+    #ejes
+    cols = list(df.columns[2:])
+    
+    #cursos
+    cursos = list(df['curso'].unique())
+    cursos_comb = []
+    appended = []
+    for c_0 in cursos:
+        for c_1 in cursos:
+            if c_0 != c_1 and c_1 not in appended:
+                cursos_comb.append([c_0, c_1])
+        appended.append(c_0)
+        
+    comb_dict = defaultdict(lambda: defaultdict())
+    for col in cols:
+        for comb in cursos_comb:
+            df1 = df[df['curso'] == comb[0]]
+            df2 = df[df['curso'] == comb[1]]
+            p_val = ttest_ind(df1.loc[:, col], df2.loc[:,col]).pvalue
+            comb_dict[col][f"{comb[0]} vs {comb[1]}"] =  "Significativo" if p_val < 0.05 else "No significativo"
+        
+        sub_df = df.loc[:, ['curso', col]].reset_index()
+        sub_df = sub_df.set_index(['index', 'curso'])[col].unstack().reset_index(drop=True)
+        sub_cols = sub_df.columns
+        fig = ff.create_distplot([sub_df[c].dropna() for c in sub_cols], sub_cols, show_hist=False, show_rug=True)
+        comb_dict[col]['kde'] = fig   
+    
+    return comb_dict
 
 def get_2fold_comparison(df1: pd.DataFrame, df1_curso: str, df2: pd.DataFrame, df2_curso: str) -> dict:
     
